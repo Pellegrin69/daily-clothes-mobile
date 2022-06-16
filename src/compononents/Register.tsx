28f658a464ad07e8f8036237c1dc3294f468a620
pellegrin.pierre <pierre.pellegrin@esqese.ucly.fr>
2022-06-17 00:34:53 +0200

    register fonctionnel

--- src/compononents/Register.tsx
+++ src/compononents/Register.tsx
@@ -1,16 +1,34 @@
 import axios from 'axios';
+import {User} from "../models/User";
+import {NavigationProp} from "@react-navigation/native";
+import {RootStackParamList} from "../RootStackParamList";
+import * as SecureStore from "expo-secure-store";
 
+type RegisterResponse = {
+    jwt: string
+    user: User
+}
 
-axios
-    .post('http://localhost:1337/auth/local/register', {
-        username: 'Kapman',
-        email: 'test@test.com',
-        password: 'Password',
-    })
-    .then(response => {
-        console.log('User profile', response.data.user);
-        console.log('User token', response.data.jwt);
-    })
-    .catch(error => {
-        console.log('An error occurred:', error.response);
-    });
+const API_URL = "http://192.168.1.34:1337/api/auth/local";
+
+export const register = (username: string, email:string, password: string, navigation: NavigationProp<RootStackParamList>) => {
+    axios
+        .post(API_URL + "/register", {
+            username: username,
+            email: email,
+            password: password,
+        })
+        .then(response => {
+            const registerResponse: RegisterResponse = response.data
+            saveKeyValuePair("userInfo", JSON.stringify((registerResponse.user)))
+            saveKeyValuePair("userToken", registerResponse.jwt)
+            navigation.navigate("Home")
+        })
+        .catch(error => {
+            console.log('An error occurred:', error.response);
+        });
+}
+
+const saveKeyValuePair = async (key: string, value: string) => {
+    await SecureStore.setItemAsync(key, value);
+}
--- src/screens/RegisterScreen.tsx
+++ src/screens/RegisterScreen.tsx
@@ -1,16 +1,32 @@
 import {NavigationProp} from "@react-navigation/native";
 import {RootStackParamList} from "../RootStackParamList";
-import React, {FunctionComponent} from "react";
-import {Button, SafeAreaView, ScrollView} from "react-native";
+import React, {FunctionComponent, useState} from "react";
+import {Button, SafeAreaView, ScrollView, TextInput} from "react-native";
+import {register} from "../compononents/Register";
 
 type Props = {
     navigation: NavigationProp<RootStackParamList>;
 }
 
 export const RegisterScreen: FunctionComponent<Props> = ({navigation}) => {
+
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
         <SafeAreaView style={{height: '100%'}}>
             <ScrollView style={{height: '100%'}}>
+                <TextInput onChangeText={ text => setUsername(text) }
+                           value={username}
+                           placeholder="Xyz123"/>
+                <TextInput onChangeText={ text => setEmail(text) }
+                           value={email}
+                           placeholder="example@gmail.com"/>
+                <TextInput onChangeText={ text => setPassword(text) }
+                           value={password}
+                           placeholder="password"/>
+                <Button title="Login" onPress={() => register(username, email, password, navigation)} />
             </ScrollView>
         </SafeAreaView>
     );
